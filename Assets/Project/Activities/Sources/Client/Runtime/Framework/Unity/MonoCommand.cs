6f40bf653af79cf4219c4cedead14190510783e4
В'ячеслав <suzium.trap@gmail.com>
2024-09-28 20:19:19 +0300

    Added Command and its core realizations

---
+++ Assets/Project/Activities/Sources/Client/Runtime/Framework/Unity/MonoCommand.cs
@@ -0,0 +1,15 @@
+﻿using Client.Runtime.Framework.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Sources.Client.Runtime.Framework.Unity
+{
+    public abstract class MonoCommand : MonoBehaviour, ICommand
+    {
+        public abstract void Execute();
+    }
+}
