dea19ced63f33cc1c22ec3e250bcef117223f7b0
В'ячеслав <suzium.trap@gmail.com>
2024-09-28 19:15:39 +0300

    Misc: Added Bootstrap scripts to load lobby scene, added asmdef file to use Zenject

---
+++ Assets/Project/Activities/Sources/Client/Runtime/System/Scenes.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.Contracts;
+
+namespace Client.Runtime.System
+{
+    public sealed class Scenes
+    {
+        public static class System
+        {
+            public const string Audio = "System_Audio";
+            public const string Input = "System_Input";
+            public const string MainCamera = "System_MainCamera";
+        }
+
+        public static class Activity
+        {
+            public const string Lobby = "Activity_Lobby";
+        }
+    }
+}
