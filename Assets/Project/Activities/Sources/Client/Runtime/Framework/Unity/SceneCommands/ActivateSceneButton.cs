07e01d659dfa29676cca8381a32bb28689e2a47d
В'ячеслав <suzium.trap@gmail.com>
2024-10-02 22:14:08 +0300

    feat: added more specific abstract button commands classes

---
+++ Assets/Project/Activities/Sources/Client/Runtime/Framework/Unity/SceneCommands/ActivateSceneButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Client.Runtime.Framework.Unity.SceneCommands
+{
+    public abstract class ActivateSceneButton : ChangeSceneButtonCommand
+    {
+        protected override List<string> ScenesToUnload => new() { };
+    }
+}
---
+++ Assets/Project/Activities/Sources/Client/Runtime/Framework/Unity/SceneCommands/ChangeSceneButtonCommand.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Client.Runtime.Framework.Unity.SceneCommands
+{
+    public abstract class ChangeSceneButtonCommand : ButtonCommand
+    {
+        private AsyncOperation _operation;
+        abstract protected string SceneToActivate { get; }
+        abstract protected List<string> ScenesToUnload { get; }
+
+        public override void Execute()
+        {
+            if (_operation != default)
+            {
+                return;
+            }
+
+            Scene gameScene = SceneManager.GetSceneByName(SceneToActivate);
+            if (gameScene != default && gameScene.isLoaded)
+            {
+                return;
+            }
+
+            _operation = SceneManager.LoadSceneAsync(SceneToActivate, LoadSceneMode.Additive);
+            _operation.completed += ActivateScene;
+        }
+
+        private void ActivateScene(AsyncOperation operation)
+        {
+            ScenesToUnload.ForEach(s => SceneManager.UnloadSceneAsync(s));
+            
+            Scene gameScene = SceneManager.GetSceneByName(SceneToActivate);
+            if (gameScene != default)
+            {
+                SceneManager.SetActiveScene(gameScene);
+            }
+            
+            Complete();
+        }
+
+        private void Complete()
+        {
+            if (_operation != default)
+            {
+                _operation.completed -= ActivateScene;
+            }
+            _operation = default;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Complete();
+        }
+    }
+}
