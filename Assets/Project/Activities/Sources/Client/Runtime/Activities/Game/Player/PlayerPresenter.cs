65b66186cd05fb681508044d77e4cf4f27abb30c
В'ячеслав <suzium.trap@gmail.com>
2024-10-04 21:28:04 +0300

    feat: added MVP pattern for player

---
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/Game/Player/PlayerPresenter.cs
@@ -0,0 +1,27 @@
+﻿
+using Client.Runtime.Framework.Presenter;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Zenject;
+
+namespace Client.Runtime.Activities.Game.Player
+{
+    public sealed class PlayerPresenter : MonoPresenter
+    {
+        [SerializeField] private PlayerView _playerView;
+
+        [Inject] private readonly PlayerModel _playerModel;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(_playerView, "[PlayerPresenter] PlayerView is required");
+        }
+        public override void Enable()
+        {
+        }
+
+        public override void Disable()
+        {
+        }
+    }
+}
---
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/Game/Player/PlayerView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Client.Runtime.Activities.Game.Player
+{
+    public sealed class PlayerView : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        public Rigidbody2D Rigidbody => _rigidbody;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(_rigidbody, "[PlayerView] Rigidbody is required");
+        }
+    }
+}
