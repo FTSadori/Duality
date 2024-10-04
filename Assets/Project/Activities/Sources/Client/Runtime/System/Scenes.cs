54168b28fac2c5ed5156f37fdbc41478c6052079
В'ячеслав <suzium.trap@gmail.com>
2024-10-04 21:21:36 +0300

    feat: added new in-game scene for game gui

--- Assets/Project/Activities/Sources/Client/Runtime/System/Scenes.cs
+++ Assets/Project/Activities/Sources/Client/Runtime/System/Scenes.cs
@@ -19,9 +19,10 @@ namespace Client.Runtime.System
             public const string Settings = "Activity_Settings";
             public const string Extra = "Activity_Extra";
             public const string ExitGamePopUp = "Activity_ExitGamePopUp";
-            public const string Tutorial = "Activity_Tutorial";
             public const string IngameMenu = "Activity_IngameMenu";
             public const string ExitToLobbyPopUp = "Activity_ExitToLobbyPopUp";
+            public const string GameGUI = "Activity_GameGUI";
+            public const string Tutorial = "Activity_Tutorial";
         }
     }
 }
