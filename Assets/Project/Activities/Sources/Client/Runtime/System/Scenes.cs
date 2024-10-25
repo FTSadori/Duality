5eb82ce3ac3d889456661ce1aa6cd23e99750a95
В'ячеслав <suzium.trap@gmail.com>
2024-10-25 22:48:10 +0300

    refactor: refactored scenes

--- Assets/Project/Activities/Sources/Client/Runtime/System/Scenes.cs
+++ Assets/Project/Activities/Sources/Client/Runtime/System/Scenes.cs
@@ -15,14 +15,10 @@ namespace Client.Runtime.System
         {
             public const string LobbyBackground = "Activity_LobbyBackground";
             public const string Lobby = "Activity_Lobby";
-            public const string Saves = "Activity_Saves";
-            public const string Settings = "Activity_Settings";
             public const string Extra = "Activity_Extra";
-            public const string ExitGamePopUp = "Activity_ExitGamePopUp";
-            public const string IngameMenu = "Activity_IngameMenu";
-            public const string ExitToLobbyPopUp = "Activity_ExitToLobbyPopUp";
             public const string GameGUI = "Activity_GameGUI";
             public const string Tutorial = "Activity_Tutorial";
+            public const string GameBootstrap = "GameBootstrap";
         }
     }
 }
