7497fb129d2bd0b0eb5872ccdf3cf7557dc7712b
В'ячеслав <suzium.trap@gmail.com>
2024-10-25 22:56:06 +0300

    feat: added general keyDownCommand that uses ButtonCommand as alternative to it

---
+++ Assets/Project/Activities/Sources/Client/Runtime/Framework/Unity/AlternativeKeyDownCommand.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Client.Runtime.Framework.Unity
+{
+    public sealed class AlternativeKeyDownCommand : KeyDownCommand
+    {
+        [SerializeField] private ButtonCommand _alternativeCommand;
+
+        public override void Execute()
+        {
+            _alternativeCommand?.Execute();
+        }
+    }
+}
