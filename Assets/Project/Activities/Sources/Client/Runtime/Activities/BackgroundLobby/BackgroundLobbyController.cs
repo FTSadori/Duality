8b19168a70c6507e0092a6bb8f797ee6f8474254
В'ячеслав <suzium.trap@gmail.com>
2024-11-14 17:07:49 +0200

    misc: used coroutines in background lobby animation

--- Assets/Project/Activities/Sources/Client/Runtime/Activities/BackgroundLobby/BackgroundLobbyController.cs
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/BackgroundLobby/BackgroundLobbyController.cs
@@ -1,17 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Client.Runtime.Activities.BackgroundLobby
 {
     public sealed class BackgroundLobbyController : MonoBehaviour
     {
         [SerializeField] private GameObject _bottomBackgroundImage;
-        private float _timer = 0;
-        private float _angleSpeed = 0.5f;
 
-        private void Update()
+        private void Awake()
         {
-            _timer += Time.deltaTime;
-            _bottomBackgroundImage.transform.localPosition = new Vector3(Mathf.Cos(_timer * _angleSpeed) * 30f, Mathf.Sin(_timer * _angleSpeed) * 30f, 0);
+            StartCoroutine(nameof(BottomBackgroundCircleMoving));
+        }
+
+        private IEnumerator BottomBackgroundCircleMoving()
+        {
+            float timer = 0;
+            float angleSpeed = 0.5f;
+
+            while (true)
+            {
+                timer += Time.deltaTime;
+                _bottomBackgroundImage.transform.localPosition = new Vector3(Mathf.Cos(timer * angleSpeed) * 30f, Mathf.Sin(timer * angleSpeed) * 30f, 0);
+                yield return null;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            StopCoroutine(nameof(BottomBackgroundCircleMoving));
         }
     }
 }
