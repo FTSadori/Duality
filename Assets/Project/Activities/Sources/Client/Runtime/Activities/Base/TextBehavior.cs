b950a5b1eb0431f73b539d759adfc21c735d1663
В'ячеслав <suzium.trap@gmail.com>
2024-11-30 00:25:54 +0200

    misc: added fonts

---
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/Base/TextBehavior.cs
@@ -0,0 +1,18 @@
+﻿using Client.Runtime.Activities.Game.ScriptableObjects;
+using UnityEngine;
+
+namespace Client.Runtime.Activities.Base
+{
+    public sealed class TextBehavior : MonoBehaviour
+    {
+        [SerializeField] FontDataScriptableObject _fontData;
+
+        private void Start()
+        {
+            if (TryGetComponent(out TMPro.TMP_Text tmptext))
+            {
+                tmptext.font = _fontData._textFont;
+            }
+        }
+    }
+}
---
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/Game/ScriptableObjects/FontDataScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Client.Runtime.Activities.Game.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "FontData", menuName = "ScriptableObjects/FontData")]
+    public sealed class FontDataScriptableObject : ScriptableObject
+    {
+        public TMP_FontAsset _textFont;
+    }
+}
