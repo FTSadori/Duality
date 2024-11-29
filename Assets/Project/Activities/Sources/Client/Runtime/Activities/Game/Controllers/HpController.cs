c52a79eb03a6a2b9b794692a7e772b8d634c60eb
В'ячеслав <suzium.trap@gmail.com>
2024-11-30 00:30:21 +0200

    feat: added hp and soul bar

---
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/Game/Controllers/HpController.cs
@@ -0,0 +1,51 @@
+﻿using Client.Runtime.Activities.Game.Player;
+using UnityEngine;
+using Zenject;
+using DG.Tweening;
+using System.Collections.Generic;
+
+namespace Client.Runtime.Activities.Game.Controllers
+{
+    public sealed class HpController : MonoBehaviour
+    {
+        [Inject] private readonly PlayerModel _playerModel;
+
+        [SerializeField] private GameObject _hpBar;
+        [SerializeField] private GameObject _hpPrefab;
+
+        private List<Animator> _hpAnimators = new();
+
+        private void Start()
+        {
+            for (int i = 0; i < _playerModel.MaxHP; ++i)
+            {
+                var obj = Instantiate(_hpPrefab, _hpBar.transform);
+                if (obj.TryGetComponent(out RectTransform rectTransform))
+                {
+                    rectTransform.anchoredPosition = new Vector3(20f + i * 30f, 0f);
+                    rectTransform.DOScale(0.8f, 0.8f).SetEase(Ease.InCubic).SetDelay(0.3f * i);
+                }
+                if (obj.TryGetComponent(out Animator animator))
+                {
+                    animator.SetBool("IsFlaming", true);
+                    _hpAnimators.Add(animator);
+                }
+            }
+
+            _playerModel.OnHPChanged += UpdateHp;
+        }
+
+        private void UpdateHp()
+        {
+            for (int i = 0; i < _playerModel.MaxHP; ++i)
+            {
+                _hpAnimators[i].SetBool("IsFlaming", (i < _playerModel.CurrentHP));
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _playerModel.OnHPChanged -= UpdateHp;
+        }
+    }
+}
---
+++ Assets/Project/Activities/Sources/Client/Runtime/Activities/Game/Controllers/SoulController.cs
@@ -0,0 +1,73 @@
+﻿using Client.Runtime.Activities.Game.Commands.ButtonCommands;
+using Client.Runtime.Activities.Game.Player;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Client.Runtime.Activities.Game.Controllers
+{
+    public class SoulController : MonoBehaviour
+    {
+        [Inject] private readonly PlayerModel _playerModel;
+
+        [SerializeField] private RectMask2D _mask;
+        [SerializeField] private Animator _tomoe;
+
+        [SerializeField] private RestartSequenceCommand _command;
+        [SerializeField] private GameObject _wholeMenuButton;
+
+        private void Start()
+        {
+            UpdateSoul();
+            _playerModel.OnSoulChanged += UpdateSoul;
+            _playerModel.OnDeath += SoulOnDeath;
+            _playerModel.OnRealDeath += OnRealDeath;
+        }
+
+        private void Update()
+        {
+            if (_playerModel.CurrentHP == 0 && _playerModel.Soul < 0.01f)
+                return;
+
+            _playerModel.Soul += 0.001f;
+        }
+
+        private void UpdateSoul()
+        {
+            float percentage = _playerModel.Soul / _playerModel.MaxSoul;
+            float size = (1f - percentage) * 60f;
+            _mask.padding = new Vector4(0f, 0f, 0f, size + 5f);
+
+            if (_playerModel.Soul == _playerModel.MaxSoul)
+            {
+                _tomoe.SetBool("IsCircling", true);
+            }
+            else
+            {
+                _tomoe.SetBool("IsCircling", false);
+            }
+        }
+
+        private void SoulOnDeath()
+        {
+            _playerModel.Soul -= 0.5f;
+        }
+
+        private void OnRealDeath()
+        {
+            _wholeMenuButton.SetActive(false);
+
+            var tween = transform.DOMoveX(0f, 1.55f);
+
+            tween.OnComplete(() => { _command.Execute(); });
+        }
+
+        private void OnDestroy()
+        {
+            _playerModel.OnSoulChanged -= UpdateSoul;
+            _playerModel.OnDeath -= SoulOnDeath;
+            _playerModel.OnRealDeath -= OnRealDeath;
+        }
+    }
+}
